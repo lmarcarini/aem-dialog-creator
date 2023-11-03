45e073098b8882556e02d4e99fa9dbbcf0aa7500
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2023-11-03 12:54:21 -0300

    feat: Implements remove node on root level only

--- src/features/TreeVisualizer/TreeVisualizer.tsx
+++ src/features/TreeVisualizer/TreeVisualizer.tsx
@@ -1,20 +1,40 @@
+import { ActionIcon } from "@mantine/core";
 import SortableTree from "@nosferatu500/react-sortable-tree";
-import styles from "./TreeVisualizer.module.css";
-import { useComponentsStore } from "../../stores/useComponentsStore";
 import "@nosferatu500/react-sortable-tree/style.css";
+import { IconTrash } from "@tabler/icons-react";
+import { useComponentsStore } from "../../stores/useComponentsStore";
 import "./TreeCustomStyle.css";
+import styles from "./TreeVisualizer.module.css";
 
 export const TreeVisualizer = () => {
   const structure = useComponentsStore((state) => state.structure);
   const setStructure = useComponentsStore((state) => state.setStructure);
+
+  const removeNode = (path: number[]) => () => {
+    if (path.length < 1) return;
+    if (path.length === 1) {
+      const newStructure = [...structure];
+      const nodeNumber = path.at(0);
+      if (nodeNumber === undefined) return;
+      newStructure.splice(nodeNumber, 1);
+      setStructure(newStructure);
+    }
+  };
 
   return (
     <div className={styles.treeWrapper}>
       <SortableTree
         className="customTree"
         treeData={structure}
-        onChange={(curStructure) => setStructure(curStructure)}
+        onChange={setStructure}
         canNodeHaveChildren={(_node) => false}
+        generateNodeProps={({ path }) => ({
+          buttons: [
+            <ActionIcon onClick={removeNode(path)}>
+              <IconTrash />
+            </ActionIcon>,
+          ],
+        })}
       />
     </div>
   );
