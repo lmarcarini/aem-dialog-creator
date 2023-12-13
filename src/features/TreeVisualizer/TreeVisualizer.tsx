66562946fd5bd1ee08c9b372ffaeb3c100fbfadb
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2023-12-12 23:55:58 -0300

    refactor: Make edit and delete buttons more concise

--- src/features/TreeVisualizer/TreeVisualizer.tsx
+++ src/features/TreeVisualizer/TreeVisualizer.tsx
@@ -2,10 +2,14 @@ import SortableTree from "@nosferatu500/react-sortable-tree";
 import "@nosferatu500/react-sortable-tree/style.css";
 import "./TreeCustomStyle.css";
 import styles from "./TreeVisualizer.module.css";
-import { DeleteButton, EditButton } from "./components";
 import { useComponentsStore } from "stores/useComponentsStore";
 
-export const TreeVisualizer = () => {
+type Props = {
+  editButton: (path: number[]) => JSX.Element | null;
+  deleteButton: (path: number[]) => JSX.Element | null;
+};
+
+export const TreeVisualizer = ({ editButton, deleteButton }: Props) => {
   const structure = useComponentsStore((state) => state.structure);
   const setStructure = useComponentsStore((state) => state.setStructure);
 
@@ -17,7 +21,7 @@ export const TreeVisualizer = () => {
         onChange={setStructure}
         canNodeHaveChildren={(_node) => false}
         generateNodeProps={({ path }) => ({
-          buttons: [<EditButton path={path} />, <DeleteButton path={path} />],
+          buttons: [editButton(path), deleteButton(path)],
         })}
       />
     </div>
