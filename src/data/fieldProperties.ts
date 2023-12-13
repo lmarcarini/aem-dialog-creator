5f8897128a2f678862fa15bfd5404f4d03390294
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2023-12-13 19:17:34 -0300

    feat: Implements password

--- src/data/fieldProperties.ts
+++ src/data/fieldProperties.ts
@@ -1,11 +1,13 @@
+import { password } from "data/fields/password";
 import { pathField } from "data/fields/pathField";
 import { switchProperties } from "data/fields/switch";
 import { textAreaProperties as textfield } from "data/fields/textArea";
 import { textFieldProperties as textarea } from "data/fields/textField";
 
 export const fieldProperties = {
-  textfield,
-  textarea,
+  password,
   pathField,
   switch: switchProperties,
+  textarea,
+  textfield,
 };
