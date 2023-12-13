4cbd0ea55b32dfe16800ca449519feec8d2ca3c6
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2023-12-13 19:37:37 -0300

    feat: Date Picker

--- src/data/fieldProperties.ts
+++ src/data/fieldProperties.ts
@@ -1,3 +1,4 @@
+import { datePicker } from "data/fields/datePicker";
 import { numberField } from "data/fields/numberField";
 import { password } from "data/fields/password";
 import { pathField } from "data/fields/pathField";
@@ -6,6 +7,7 @@ import { textAreaProperties as textfield } from "data/fields/textArea";
 import { textFieldProperties as textarea } from "data/fields/textField";
 
 export const fieldProperties = {
+  datePicker,
   numberField,
   password,
   pathField,
